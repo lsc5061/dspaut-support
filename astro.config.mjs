019d8b0b0697ba0a6bf9a69a8e7466d5db6545ac
lsc5061 <lsc@dspaut.com>
2026-06-29 13:57:40 +0900

    SEO 구글 엔진 관련 수정

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import alpinejs from '@astrojs/alpinejs';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://support.dspaut.com',
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [alpinejs()],
+  integrations: [alpinejs(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
